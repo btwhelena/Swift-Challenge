203814f4fded859ccc1474164bd69a478251bfa5
Helena <helenaoliveira@alu.ufc.br>
2022-03-11 10:52:56 -0300

    json file and struct ingredient model

--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import Foundation
+
+let data:Data
+
+print("Welcome to Kitchen Assistant! \n")
+print("Select an ingredient: \n")
+
+//Creating Ingredient model
+struct Ingredient{
+    var name: String
+    var tableSpoon: Int
+    var teaSpoon: Int
+    var teaCup: Int
+}
+
+//Convert data
