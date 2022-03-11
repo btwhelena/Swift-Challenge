a138553e5b6755dee5f50bdae4fe3db9677f3fff
Helena <helenaoliveira@alu.ufc.br>
2022-03-11 11:44:35 -0300

    creating menu and adding emojis hehehe

--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -1,18 +1,55 @@
 import Foundation
-
 let data:Data
 
-print("Welcome to Kitchen Assistant! \n")
-print("Select an ingredient: \n")
+print("🍉 Welcome to Kitchen Assistant! 🥑 \n")
+print("Select ingredient from the list below: \n")
+print("0 - Wheat Flour")
+print("1 - Sugar")
+print("2 - Corn Start")
+print("3 - Oat")
+print("4 - Chocolate powder")
+print("5 - Yeast")
+print("6 - Baking Powder")
+print("7 - Butter")
+print("8 - Milk")
+print("9 - Water")
+print("10 - Oil")
+print("Type a number: ", terminator: "")
 
-//Creating Ingredient model
+let option = readLine()
+
+/* switch option {
+    
+    case "0":
+    
+        break
+    
+    case "1":
+ 
+        break
+    
+    case "2":
+        
+        break
+    
+    case "3":
+    
+        break
+    
+    case "4":
+        
+        break
+    
+    case "5":
+        
+        break
+    
+}
+
+Creating Ingredient model
 struct Ingredient{
     var name: String
     var tableSpoon: Int
     var teaSpoon: Int
     var teaCup: Int
-}
-
-// NEW COMMIT
-
-//Convert data
+} */
